67cec5fee4a79d5512d8879d6b08c25090681aa8
Jorge <33935947+JParzival@users.noreply.github.com>
2018-09-13 14:33:31 +0200

    Update telegramMessage.py

--- telegramMessage.py
+++ telegramMessage.py
@@ -1,9 +1,10 @@
 import telepot
 
 send_telegram_msg()
+
 def send_telegram_msg():
     msg = ""
-    chatid = settings.TEL_CHATID
-    token = settings.TEL_TOKEN
+    chatid = ""
+    token = ""
     bot = telepot.Bot(token=token)
     bot.sendMessage(chat_id=chatid, text=msg)
